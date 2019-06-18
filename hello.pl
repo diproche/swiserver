fcebceef8c2b56bfc5e56f6dc1fe94eb6a9c6458
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2019-06-18 13:05:35 +0200

    Clean up and fix the error

--- hello.pl
+++ hello.pl
@@ -2,31 +2,20 @@
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_parameters)).
 :- use_module(library(http/http_client)).
+
 :- use_module(diproche8).
 
-:- http_handler(root(hallo), say_hi, []).		% (1)
-:- http_handler(root(dipr), reply, []).		% (1)
+:- http_handler(root(dipr), reply, []).
 
-server(Port) :-						% (2)
+
+
+server(Port) :-
         http_server(http_dispatch, [port(Port)]).
-
-
-transform(R) :-
-        atom_string(R, "[hallo]").
-
 
 reply(Request) :-
         member(method(post), Request), !,
-        http_read_data(Request, Data, to(atom)),
-        term_to_atom(Data, Proof),
-        diproche_fo(Proof, R),
+        http_read_data(Request, ProposedProof, []),
+        diproche_fo(ProposedProof, DiprocheReturn),
         format('Content-type: text/plain~n~n', []),
-        format(R).
-        % format(Data).
+        format(DiprocheReturn).
 
-% [[es,seien,a,und,b,aussagen],[wir,zeigen,[[a,and,b],<->,[b,and,a]]],[=>],[angenommen,[a,and,b]],[dann,a],[ferner,gilt,b],[damit,folgt,[b,and,a]],[qed],[abs],[<=],[nehmen,wir,jetzt,an,dass,[b,and,a]],[dann,folgt,a],[ausserdem,folgt,b],[also,gilt,nun,[a,and,b]],[qed],[damit,gilt,[[a,and,b],<->,[b,and,a]]]]
-say_hi(Request) :-					% (3)
-        http_parameters(Request,
-        [ name(Name, [])]),
-        format('Content-type: text/plain~n~n'),
-         format(Name).
